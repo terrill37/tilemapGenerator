97c0940134a918db0accaaecefb8c88e412d9f76
wterrill <walruswes@gmail.com>
2023-05-23 15:45:13 +0200

    removed unecessary code

--- src/main.cpp
+++ src/main.cpp
@@ -14,43 +14,6 @@ int main(){
         mapGen.Update();
         mapGen.LateUpdate();
         mapGen.Draw();
-    }
-
-    //sf::ContextSettings settings;
-    //settings.depthBits          = 24;
-    //settings.stencilBits        = 8;
-    //settings.antialiasingLevel  = 4;
-    //settings.majorVersion       = 4;
-    //settings.minorVersion       = 6;
-
-    //sf::RenderWindow  window(sf::VideoMode(200, 200), "SFML works!", sf::Style::Default, settings);//, sf::Style::Titlebar);
-    //sf::CircleShape shape(100.f);
-    //shape.setFillColor(sf::Color::Green);
-    
-    //std::cout<<"OpenGL version: "<<settings.majorVersion<<"."<<settings.minorVersion<<std::endl;
-
-    //while(window.isOpen()){
-    //    sf::Event event;
-    //    while(window.pollEvent(event)){
-    //        switch(event.type){
-    //            case sf::Event::Closed:
-    //                window.close();
-    //                break;
-    //            case sf::Event::KeyPressed:
-    //                if(event.key.code==sf::Keyboard::Escape){
-    //                    window.close();
-    //                    std::cout<<"closing window: Esc pressed\n";
-    //                }
-    //                break;
-    //            default:
-    //                break;
-    //        }
-    //        
-    //    }
-    //    window.clear();
-    //    window.draw(shape);
-    //    window.display();
-    //}
-    
+    }    
     return 0;
 }
